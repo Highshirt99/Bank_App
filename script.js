884c4b4dd5dd3361df45679f4df884efb53d4f3e
Highshirt99 <95690953+Highshirt99@users.noreply.github.com>
2022-03-21 14:57:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/* let container1 = document.querySelector(".container1");
+let container2 = document.querySelector(".container2");
+container2.style.display = "none";
+function goToHome() {
+    container1.style.display = "none";
+    container2.style.display = "block";
+ }*/
