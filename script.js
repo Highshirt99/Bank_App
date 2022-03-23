401846ec2ca0509bd7767ef44136f2d4d1cfced4
Highshirt99 <95690953+Highshirt99@users.noreply.github.com>
2022-03-23 10:40:31 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-/* let container1 = document.querySelector(".container1");
-let container2 = document.querySelector(".container2");
-container2.style.display = "none";
-function goToHome() {
-    container1.style.display = "none";
-    container2.style.display = "block";
- }*/
